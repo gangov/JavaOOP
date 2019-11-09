41fa5ceafafc7d9fe64d3ffa302c0b89aebc0027
gangov <gangov@gmail.com>
2019-11-09 17:19:20 +0200

    almost done

---
+++ InterfacesAndAbstraction/src/carshoplab/Car.java
@@ -0,0 +1,15 @@
+package carshoplab;
+
+import java.io.Serializable;
+
+public interface Car extends Serializable {
+    Integer Tires = 4;
+
+    String getModel();
+
+    String getColor();
+
+    Integer getHorsePower();
+
+    String countryProduced();
+}
---
+++ InterfacesAndAbstraction/src/carshoplab/Main.java
@@ -0,0 +1,7 @@
+package carshoplab;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
